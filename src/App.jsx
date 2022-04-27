dd40c9d975fa90ef6555627db83e6641e3186624
Joe Daniel Ortiz Morales <isc18350328@gmail.com>
2022-04-27 10:01:10 -0500

    Tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,87 +21,101 @@ function App() {
 
   };
 
-  const handleResetClick = ( ) => {
+  const handleClickLimpiar = ( ) => {
     setInputState ({titulo: " ", fecha: " ", nota: " "});
   };
 
   const handleClickGuardar = () => {
-    localStorage.setItem("notas", JSON.stringify(inputState))
+    let arregloNotas = JSON.parse
+    (localStorage.getItem("notas")) || [];
+    arregloNotas.push(inputState)
+    localStorage.setItem("notas",JSON.
+    stringify(arregloNotas));
+    handleClickLimpiar();
   };
+
+  const handleInputClean = () => { 
+    setInputState({
+        titulo: "",
+        fecha: "",
+        nota: "",
+    });
+   }; 
     
   return (
-
     <div className="App container">
-      <div className="row">
-       <div className= "col">
-          <h3> LISTAS </h3>
+      <div className="row"> 
+        <div className="col"> 
+            <h3>Lista</h3> 
         </div>
         <div className="col">
-          <h3> NOTAS </h3>
-        <label className = 'mb-2' > 
-
-        TITULO 
-
-        <input
-          id ="titulo" 
-          name="titulo" 
-          type="text"
-          onChange={handleInputChange}
-          value={inputState.titulo}
-        />
-      <br />
+      <h3>Notas</h3>
+      <label className="mb-2" style={{ width:"100%" }}>
+        Título 
+          <input 
+            id="titulo" 
+            name="titulo" 
+            type="text" 
+            onChange={handleInputChange}
+            value={inputState.titulo}
+            style={{ width:"100%" }} 
+            />
+      </label>   
+      <br />    
+      <label className="mb-2" style={{ width:"100%" }}>
+        Fecha
+          <input 
+            id="fecha" 
+            name="fecha" 
+            type="text" 
+            onChange={handleInputChange}
+            value={inputState.fecha} 
+            style={{ width:"100%" }}
+            />          
       </label>
-
-      <label htmlFor="fecha"> FECHA </label>
-
-      <input 
-      id ="fecha" 
-      name="fecha" 
-      type="text"
-      onChange={handleInputChange}
-      value={inputState.fecha}
-
-      /><label htmlFor="nota"> NOTA </label>
-
-      <input 
-      id ="nota" 
-      name="nota" 
-      type="text"
-      onChange={handleInputChange}
-      value={inputState.nota}
-      />
-        
-        </div>
-      </div>
-      <hr />
-      <div className=" ms-2 mt-2">
-      <button 
-      type = "button" 
-      className ="btn btn-primary"
-      onClick={handleResetClick}
-      style= {{marginLeft: "5px"}}
-      >
-        RESET
-      
-      </button>
-
-      <div className="col">
+      <br />
+      <label className="mb-2" style={{ width:"100%" }}>
+        Nota
+          <input 
+            id="nota" 
+            name="nota" 
+            type="text" 
+            onChange={handleInputChange}
+            value={inputState.nota} 
+            style={{ width:"100%" }}
+            />          
+      </label>
+      <hr />         
+    <div className="mt-2 me-2 mt-2 row">
+      <div className="col"> 
       <span className="row me-1">
         <button 
-        type = "button" 
-        className ="btn btn-primary"
-        onClick={handleClickGuardar}
-        style= {{marginLeft: "5px"}}
+            type="button"
+            className="btn btn-primary" 
+            onClick={handleInputClean}
         >
-          GUARDAR
-        
-        </button>
-        </span>
+            Limpiar
+        </button> 
+      </span>
       </div>
+    <div className="col">
+      <span className="row ms-1">   
+        <button 
+            type="button" 
+            className="btn btn-primary" 
+            onClick={handleClickGuardar}
+        >
+        Guardar
+      </button>
+      </span>
+    </div>
 
+    </div>      
     </div>
     </div>
 
+
+    </div>
   );
 }
 
